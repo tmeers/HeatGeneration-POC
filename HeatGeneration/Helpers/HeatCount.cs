5a08cf3a7778fd16db329dc9416c0e891da4ec86
Tim Meers <tim.meers@gmail.com>
2017-02-05 20:54:02 -0500

    Updated how heat counts are counted

--- HeatGeneration/Helpers/HeatCount.cs
+++ HeatGeneration/Helpers/HeatCount.cs
@@ -6,5 +6,25 @@
         {
             return (racersCount * laneCount) / raceCount;
         }
+
+        public static int GenerateHeatCount(int laneCount, int racersCount)
+        {
+            if (laneCount == 3 && (racersCount <= 2))
+                return laneCount;
+
+            if (laneCount == 3 && (racersCount >= 3))
+                return racersCount;
+
+            if (laneCount == 4)
+                return racersCount;
+
+            if (laneCount == 6 && ((racersCount <= 7 && racersCount > 3) || racersCount == 9 || racersCount == 10 || racersCount == 12))
+                return racersCount;
+
+            if (laneCount == 6 && (racersCount <= 3))
+                return 4;
+
+            return laneCount;
+        }
     }
 }
